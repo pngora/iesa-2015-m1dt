52861c42a5cc379aa894a032bf3c5b69620115a8
unknown <p.ngora@gmail.com>
2015-03-19 12:45:28 +0100

    rajout signature de commande

---
+++ 06_php/01_helloworld/exercice.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<?php
+	/*$a =1;;
+	$b =2;;
+	function somme(){
+		global $a, $b;;
+		$b = $a + $b;;
+	}
+somme();;
+echo $b();;*/
+		
+	/*function somme($a,$b){
+		
+		return $b = $a + $b;;
+	}
+echo somme(1,2);;*/
+
+	$a =1;;
+	$b =2;;
+	function somme($a,$b){
+		
+		return $b = $a + $b;;
+	}
+echo somme($a,$b);;
+
+?>
+</body>
+</html>
---
+++ 06_php/02_rss/exercice flux rss.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+
+
+	<?php 
+		$a="112";
+		var_dump($a);
+		
+	?>
+</body>
+</html>
