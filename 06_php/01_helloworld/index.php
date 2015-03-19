dcbb6463afb91e3ffd0b473f28593fa0556867f5
unknown <p.ngora@gmail.com>
2015-03-19 14:44:39 +0100

    les tableaux en php

--- 06_php/01_helloworld/index.php
+++ 06_php/01_helloworld/index.php
@@ -4,20 +4,17 @@
 </head>
 <body>
 	<?php 
-		$course=array(
-			"fruit"=>"orange",
-		    "fruit2"=>"kiwi"
-		);
-
-		var_dump($course);
-		
-	?>
-
-
-	<?php 
-		$a="112";
-		var_dump($a);
-		
+		$fluxRSS = array(
+				"title" =>"Le Monde.fr - Actualité à la Une",
+				"en"=>"en",
+				"description"=>"Le Monde.fr - 1er site d'information. Les articles du journal et toute l'actualité en continu : International, France, Société, Economie, Culture, Environnement, Blogs ...",
+				"link"=>"http://www.lemonde.fr/rss/une.xml",
+				"copyright"=>"Copyright Le Monde.fr"
+				"pubDate"=>"Thu, 19 Mar 2015 09:46:51 GMT",
+				"lastBuildingDate"=>"Thu, 19 Mar 2015 09:46:51 GMT",
+				"ttl"=>"15"
+			);;
+		var_dump($fluxRSS)
 	?>
 </body>
 </html>
--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -3,7 +3,40 @@
 <head>
 </head>
 <body>
+	<?php 
+		$Tunisiens = array(
 
+				"title" =>"Des Tunisiens manifestent pour dénoncer l'attaque sanglante",
+				"en"=>"en",
+				"description"=>"Plusieurs centaines de Tunisiens ont commencé à se rassembler mercredi soir à Tunis pour dénoncer l'attaque au Musée du Bardo.&lt;img width='1' height='1' src='http://rss.lemonde.fr/c/205/f/3050/s/44904fc2/sc/11/mf.gif'",
+				"link"=>"http://www.lemonde.fr/afrique/video/2015/03/19/des-tunisiens-manifestent-pour-denoncer-l-attaque-sanglante_4596526_3212.html#xtor=RSS-3208",
+				"copyright"=>"Copyright Le Monde.fr",
+				"date"=>"Thu, 19 Mar 2015 10:16:44 GMT"
 
+			);;
+
+		$salonDuLivre = array(
+
+				"title" =>"Le Salon du livre dans le regard de Micaël",
+				"en"=>"en",
+				"description"=>"En six dessins à l’humour faussement blasé, l’artiste argentin croque le grand raout parisien des lettres.",
+				"link"=>"http://www.lemonde.fr/bande-dessinee/article/2015/03/19/le-salon-du-livre-dans-le-regard-de-micael_4596493_4420272.html#xtor=RSS-3208",
+				"copyright"=>"Copyright Le Monde.fr",
+				"date"=>"Thu, 19 Mar 2015 10:16:44 GMT"
+
+			);;
+
+		$salonDuLivre = array(
+
+				"title" =>"Tunisie : l'un des deux assaillants était surveillé par les services de sécurité",
+				"en"=>"en",
+				"description"=>"Les deux assaillants s'appelaient Yassine Abidi et Hatem Khachnaoui, affirme Habib Essid, le premier ministre tunisien, qui précise qu'Abidi était « connu des services de sécurité et suivi",
+				"link"=>"http://www.lemonde.fr/afrique/article/2015/03/19/tunisie-les-assaillants-ont-ete-identifies_4596469_3212.html#xtor=RSS-3208",
+				"copyright"=>"Copyright Le Monde.fr",
+				"date"=>"Thu, 19 Mar 2015 10:16:44 GMT"
+
+			);;
+$Tunisiens["title"]
+	?>
 </body>
 </html>
