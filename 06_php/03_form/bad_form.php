48b466a91fde794c2e038878992cf43189b1a953
unknown <p.ngora@gmail.com>
2015-03-20 10:18:47 +0100

    Faire un formulaire en php dans la même page

--- 06_php/03_form/bad_form.php
+++ 06_php/03_form/bad_form.php
@@ -8,18 +8,18 @@
 		 echo $date = date("d-m-Y"); 
 	 ;?>
 
-	<form method ="post" action="post.php">
+	<form method ="post" action="form.php">
 		<p>Pr&eacutenom : <input type="text" name="prenom"/></p>
 		<p>Nom : <input type="text" name="nom"/><p/>
 		<input type="submit" value="Envoyer"/>
 	</form>
-	<?php/*
+	<?php
 		if($_GET){
 			 echo $_GET['prenom'].$_GET['nom'];
 		}
 		else{
 
 		}
-	*/?>
+	?>
 </body>
 </html>
